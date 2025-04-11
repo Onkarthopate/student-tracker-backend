fc0bf9b18230353d9b35591dfde2a2ae46e531aa
Omkara <omkarthopate1432@gmail.com>
2025-04-11 10:21:41 +0530

    update API base URL and rebuild

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const cors = require("cors");
+const cors = require('cors');
 const connectDB = require("./config/db");
 
 dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors({ origin: "https://student-tracker-app-one.vercel.app" }));
+app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(express.json());
 
 app.use("/api/jobs", require("./routes/jobRoutes"));
